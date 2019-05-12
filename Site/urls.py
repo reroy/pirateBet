04d308b25acbf88fd173210dc41ec6aa906d7b49
reroy <reroy@abv.bg>
2019-05-12 20:13:19 +0300

    Migrate to Django 2.1.2

--- Site/urls.py
+++ Site/urls.py
@@ -6,15 +6,13 @@ from django.contrib.auth import views as auth_views
 import events
 
 urlpatterns = [
-    url(r'^events/', include('events.urls', namespace='events')),
-    url(r'^teams/', include('teams.urls', namespace='teams')),
+    url(r'^events/', include(('events.urls', 'events'), namespace='events')),
+    url(r'^teams/', include(('teams.urls', 'teams'), namespace='teams')),
     url(r'^home/$', events.views.home, name='home'),
     url(r'^cash/$', events.views.credit_card, name='cash'),
     url(r'^admin/', admin.site.urls),
 
     url(r'^signup/$', events.views.signup, name='signup'),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'home'}, name='logout'),
+    url(r'^login/$', auth_views.LoginView, name='login'),
+    url(r'^logout/$', auth_views.LogoutView, {'next_page': 'home'}, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
